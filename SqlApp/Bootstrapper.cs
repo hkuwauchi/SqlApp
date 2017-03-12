e58da5ff026c9a86354603cb4e4ced0f1b9f9a6d
Hisanori Kuwauchi <hkuwauchi@gmail.com>
2017-03-12 09:04:08 +0900

    プロジェクト ファイルを追加します。

---
+++ SqlApp/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using SqlApp.Views;
+using System.Windows;
+
+namespace SqlApp
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
